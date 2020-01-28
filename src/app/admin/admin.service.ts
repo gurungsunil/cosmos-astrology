6cf75b3745b6985737164a09c8c695cbe8c5854d
praveengitsit <praveenbhusal97@gmail.com>
2020-01-28 16:08:59 +0545

    Question Price

--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject, Observable, forkJoin } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { AdminMessage } from './admin-settings/admin-messages/admin-messages.model';
+import { QuestionPrice } from './admin-settings/pricing/pricing.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class AdminService {
 
   private ROOT_URL = environment.baseUrl;
   private ADMIN_MESSAGES = `${this.ROOT_URL}/admin/messages`;
+  private QUESTION_PRICE = `${this.ROOT_URL}/admin/question-price`;
 
   public addModeratorResponse = new BehaviorSubject(null);
   addModeratorResponse$ = this.addModeratorResponse.asObservable();
@@ -19,6 +21,8 @@ export class AdminService {
   addAstrologerResponse$ = this.addAstrologerResponse.asObservable();
 
   constructor(private http: HttpClient) { }
+
+  // MESSAGE START 
 
   getMessage(messageType): Observable<AdminMessage> {
     return this.http.get<AdminMessage>(
@@ -41,5 +45,21 @@ export class AdminService {
     return forkJoin([welcomeMessageResponse, systemMessageResponse]);
   }
 
+  // MESSAGE END 
+
+  // PRICING START 
+  getQuestionPrice(): Observable<QuestionPrice[]> {
+    return this.http.get<QuestionPrice[]>(
+      this.QUESTION_PRICE
+    );
+  }
+  // PRICING END 
+
+  saveQuestionPrice(questionPriceModel: QuestionPrice): Observable<QuestionPrice> {
+    return this.http.post<QuestionPrice>(
+      this.QUESTION_PRICE,
+      questionPriceModel
+    );
+  }
 
 }
