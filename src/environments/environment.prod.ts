e745a20c8ec056eeab61f70930df1cceec562ffb
praveengitsit <praveenbhusal97@gmail.com>
2020-07-10 15:39:53 +0545

    Dashboard Report

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://193.37.152.233:8080/api'
+  // baseUrl: 'http://193.37.152.233:8080/api',
+  baseUrl: 'http://d72e13dd141c.ngrok.io/api'
 };
