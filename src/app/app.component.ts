6637f16f516df90010ced34ec5a0fed1af891b0e
praveengitsit <praveenbhusal97@gmail.com>
2020-07-22 14:34:16 +0545

    UI fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthenticationService } from "./auth/authentication.service";
-import { Router, NavigationEnd, NavigationStart } from "@angular/router";
+import { Router, NavigationEnd, NavigationStart, RouterOutlet } from "@angular/router";
 import { AppService } from "./app.service";
 import { hammerjs } from 'node_modules/hammerjs';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   title = "EventusEventRegistrationV1-frontend";
